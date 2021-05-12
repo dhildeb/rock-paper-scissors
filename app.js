1a484e2578e09583f771ceeec1e96d4b27583c1e
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-05-12 11:56:02 -0600

    rps

--- app.js
+++ app.js
@@ -2,49 +2,61 @@ let ties = 0
 let wins = 0
 let loses = 0
 
-function play(playerchoice) {
+function play(playerChoice) {
+  if (typeof playerChoice != 'string') {
+    throw new Error(`${playerChoice} is not a string`)
+  }
 
   let computerChoice = Math.floor(Math.random() * 3) + 1
 
-  if (playerchoice == "rock") {
+  if (playerChoice == "rock") {
     if (computerChoice == 1) {
       ties++
-      window.alert("Rock vs Rock, You Tied")
+      drawComputerRock()
+      drawTies()
     } else if (computerChoice == 2) {
       loses++
-      window.alert("Rock vs Paper, You lose!")
+      drawComputerPaper()
+      drawLoses()
     } else if (computerChoice == 3) {
       wins++
-      window.alert("Rock vs Scissors, You Win!")
+      drawWins()
+      drawComputerScissors()
     }
+    drawRock()
   }
-  else if (playerchoice == "paper") {
+  else if (playerChoice == "paper") {
     if (computerChoice == 1) {
       wins++
-      window.alert("Paper vs Rock, You Win!")
+      drawWins()
+      drawComputerRock()
     } else if (computerChoice == 2) {
       ties++
-      window.alert("Paper vs Paper, You Tied")
+      drawTies()
+      drawComputerPaper()
     } else if (computerChoice == 3) {
       loses++
-      window.alert("Paper vs Scissors, You Lose!")
+      drawLoses()
+      drawComputerScissors()
     }
+    drawPaper()
   }
-  else if (playerchoice == "scissors") {
+  else if (playerChoice == "scissors") {
     if (computerChoice == 1) {
       loses++
-      window.alert("Scissors vs Rock, You Lose!")
+      drawLoses()
+      drawComputerRock()
     } else if (computerChoice == 2) {
       wins++
-      window.alert("Scissors vs Paper, You Win!")
+      drawWins()
+      drawComputerPaper()
     } else if (computerChoice == 3) {
-      tied++
-      window.alert("Scissors vs Scissors, You Tied")
+      ties++
+      drawTies()
+      drawComputerScissors()
     }
+    drawScissors()
   }
-  drawWins()
-  drawLoses()
-  drawTies()
 }
 
 function drawWins() {
@@ -52,16 +64,61 @@ function drawWins() {
   Wins: ${wins}
   `
   document.getElementById("wins").innerHTML = template
+  template = `
+  You Win!
+  `
+  document.getElementById("result").innerHTML = template
+
 }
 function drawTies() {
   template = `
   Ties: ${ties}
   `
   document.getElementById("ties").innerHTML = template
+  template = `
+  You Tied
+  `
+  document.getElementById("result").innerHTML = template
 }
 function drawLoses() {
   template = `
   Loses: ${loses}
   `
   document.getElementById("loses").innerHTML = template
+  template = `
+  You Lose!
+  `
+  document.getElementById("result").innerHTML = template
+}
+
+function drawRock() {
+  document.getElementById("player-img").classList.remove("paper")
+  document.getElementById("player-img").classList.remove("scissors")
+  document.getElementById("player-img").classList.add("rock")
+}
+function drawPaper() {
+  document.getElementById("player-img").classList.remove("scissors")
+  document.getElementById("player-img").classList.remove("rock")
+  document.getElementById("player-img").classList.add("paper")
+}
+function drawScissors() {
+  document.getElementById("player-img").classList.remove("paper")
+  document.getElementById("player-img").classList.remove("rock")
+  document.getElementById("player-img").classList.add
+    ("scissors")
+}
+function drawComputerRock() {
+  document.getElementById("computer-img").classList.remove("computer-paper")
+  document.getElementById("computer-img").classList.remove("computer-scissors")
+  document.getElementById("computer-img").classList.add("computer-rock")
+}
+function drawComputerPaper() {
+  document.getElementById("computer-img").classList.remove("computer-scissors")
+  document.getElementById("computer-img").classList.remove("computer-rock")
+  document.getElementById("computer-img").classList.add("computer-paper")
+}
+function drawComputerScissors() {
+  document.getElementById("computer-img").classList.remove("computer-paper")
+  document.getElementById("computer-img").classList.remove("computer-rock")
+  document.getElementById("computer-img").classList.add("computer-scissors")
 }
